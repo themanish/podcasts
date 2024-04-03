2063402a5ecbbdab453cf08d7e00164acd25a153
Manish Shrestha <manishrestha529@gmail.com>
2024-04-03 20:24:07 +0100

    add template and fix date

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 from gtts import gTTS
-from datetime import date
+import datetime
 from jinja2 import Environment, FileSystemLoader
 import os
 import json
@@ -18,20 +18,28 @@ for index, ticker in tickers.tickers.items():
 
     script += f"The {longName} is {status} by {percentChange} percent."
 
-date=str(date.today())
+date=str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M"))
 gTTS(text=script, lang='en').save(f"../mp3/{date}.mp3")
 
-
-
+# update storage
 with open('../local_storage.json', 'r') as file:
     json_data = file.read()
 
 podcasts = json.loads(json_data)
-podcasts.append({'date': date, 'size': os.stat(f"../mp3/{date}.mp3").st_size})
+podcasts.append({
+    'title': date,
+    'datetime': str(datetime.datetime.now().strftime("%A, %d %b %Y %H:%M GMT")), 
+    'size': os.stat(f"../mp3/{date}.mp3").st_size
+})
 
 json_data = json.dumps(podcasts)
 with open('../local_storage.json', 'w') as file:
     file.write(json_data)
+
+# update index.html
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template("index.template.html")
+template.stream(podcasts=podcasts).dump('../index.html')
 
 # publish rss
 env = Environment(loader=FileSystemLoader('.'))
